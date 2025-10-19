c5aa5cbc2d18064a55b6cfdb623550ff375c0bf3
Aman | امان <60558994+aman-arabzadeh@users.noreply.github.com>
2025-10-19 17:48:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,17 @@
 from fpdf import FPDF
 
-# Create instance of FPDF class
+
 pdf = FPDF()
 
-# Add a page
 pdf.add_page()
 
-# Set font
+
 pdf.set_font("Arial", size=12)
 
-# Add a cell
 pdf.cell(200, 10, txt="Ernest Hemingway", ln=True, align='C')
 
 
-# All you have to do is write one true sentence. Write the truest sentence that you know.
 pdf.multi_cell(0, 10, txt='"All you have to do is write one true sentence. Write the truest sentence that you know."')
 
-# Save the pdf with name .pdf
 pdf.output("ErnestHemingway.pdf")
 
